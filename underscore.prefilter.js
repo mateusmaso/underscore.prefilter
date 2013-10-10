b2a2ba71b56c9f1879bf763bc32047c4fe435308
Mateus Maso <m.maso25@gmail.com>
2013-10-10 10:19:11 -0700

    Created underscore.prefilter.js

---
+++ underscore.prefilter.js
@@ -0,0 +1,11 @@
+(function(_) {
+
+  _.prefilter = function(method, filter) {
+    return function() {
+      if (filter.apply(this, arguments)) {
+        return method.apply(this, arguments);
+      }
+    };
+  };
+
+})(_);
